c627274bb860e0e2de5b2d79d02d94024dde25b0
yaetoti <turkmencheburek0.o@gmail.com>
2024-08-06 11:56:12 +0300

    Add Hologram cubes.

--- src/Engine/Flame/engine/MeshSystem.cpp
+++ src/Engine/Flame/engine/MeshSystem.cpp
@@ -47,8 +47,14 @@ namespace Flame {
     {
       auto model = m_hologramGroup.AddModel(ModelManager::Get()->GetModel("Assets/Models/Samurai/Samurai1.obj"));
       auto material1 = model->AddMaterial({});
-      material1->AddInstance({ Transform { glm::vec3(0.0f), glm::vec3(0.5f) }, glm::vec3(0, 1, 1), glm::vec3(1, 0, 0) });
-      material1->AddInstance({ Transform { glm::vec3(1.25f, 0.0f, 0.0f) }, glm::vec3(0, 1, 0), glm::vec3(0, 1, 1) });
+      material1->AddInstance({ Transform(glm::vec3(0.0f), glm::vec3(0.5f)), glm::vec3(0, 1, 1), glm::vec3(1, 0, 0) });
+      material1->AddInstance({ Transform(glm::vec3(1.25f, 0.0f, 0.0f)), glm::vec3(0, 1, 0), glm::vec3(0, 1, 1) });
+      
+      auto model2 = m_hologramGroup.AddModel(ModelManager::Get()->GetModel("Assets/Cube.obj"));
+      auto material2 = model2->AddMaterial({});
+      material2->AddInstance({ Transform(glm::vec3(0.0f, -8.0f, 0.0f), glm::vec3(2.5f)), glm::vec3(1, 0, 1), glm::vec3(1, 0, 1) });
+      material2->AddInstance({ Transform(glm::vec3(-8.0f, 0.0f, 0.0f), glm::vec3(3.5f)), glm::vec3(1, 0, 0), glm::vec3(1, 0, 0) });
+      material2->AddInstance({ Transform(glm::vec3(0.0f, 0.0f, -8.0f), glm::vec3(1.5f)), glm::vec3(0, 0, 1), glm::vec3(0, 0, 1) });
     }
 
     m_opaqueGroup.Init();
